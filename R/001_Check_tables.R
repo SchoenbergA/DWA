a72b5bcc7e09b330a88dce6569b2efb6462dd9ee
Envimaster@combat <Schoenb8@staff.uni-marburg.de>
2023-06-19 19:00:05 +0200

    update doku

--- R/001_Check_tables.R
+++ R/001_Check_tables.R
@@ -1,7 +1,7 @@
 ### Check merged Table Level 3 cleaning Unsure and missing values
 
 # set environment paths
-wd <- "C:/Envimaster/DWA" # local path to repository
+wd <- "C:/Envimaster/DWA/Data" # local path to repository
 dat <- file.path(wd,"Data")
 r <- file.path(wd,"R")
 
@@ -10,36 +10,32 @@ require(openxlsx)
 source(file.path(r,"DWA_status_functions.R"))
 
 # load tables
-old <- read.xlsx(file.path(wd,"table_for_control.xlsx")) # equal to act stand aus allen 4 tabs
-new <- read.xlsx(file.path(wd,"table_for_control_AL.xlsx")) # weniger NA als old, aber auch mismatches
-cle <- read.xlsx(file.path(wd,"DWA_unsure_cleaned.xlsx")) # unbekannt welcher stagte das ist
-tes <- read.xlsx(file.path(wd,"table_for_control3.xlsx")) # act stand aus allen 4 tabs
+old <- read.xlsx(file.path(wd,"DWA_Level_2.xlsx")) # equal to act stand aus allen 4 tabs
+new <- read.xlsx(file.path(wd,"DWA_Level_3.xlsx")) # weniger NA als old, aber auch mismatches
+cle <- read.xlsx(file.path(wd,"DWA_Level_1_unsure_cleaned.xlsx")) # unbekannt welcher stagte das ist
+
 # all data sorted by hand in order by DigiIndex
 
 # check if order is equal
 head(cle)[1:5]
 head(old)[1:5]
 head(new)[1:5]
-head(tes)[1:5]
 
 # check NA
 cols<- 1:ncol(df)
 cols <-c(25:40,42:50,52,54:56)
-check_NA(new,cols)
-check_NA(tes,cols)
-check_NA(old,cols)
 check_NA(cle,cols)
+check_NA(old,cols)
+check_NA(new,cols)
 
 # check unsure
+check_unsure(cle)
 check_unsure(old)
 check_unsure(new)
-check_unsure(cle)
-check_unsure(tes)
 
 # all,equal test
 all.equal(old,new)
 all.equal(cle,old)
-all.equal(tes,old)# no diff
 all.equal(new,cle)
 
 
